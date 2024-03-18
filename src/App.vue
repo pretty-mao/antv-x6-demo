950e682fe4b8688da586f75a48c1084ed6764312
刘志伟 <2055572879@qq.com>
2024-03-18 13:44:26 +0800

    x6

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,41 @@
 <template>
-  <div class="hello">
-    <div class="mermaid">
-      graph TD;
-        A-->B;
-        A-->C;
-        B-->D;
-        C-->D;
-    </div>
-  </div>
+  <div ref="container"></div>
 </template>
 
 <script>
-import mermaid from 'mermaid';
+import { Graph } from '@antv/x6'
 
 export default {
-  name: "Hello",
-  data() {
-    return {
-      value: "Hello Vite Vue2",
-    };
-  },
+  name: 'App',
   mounted() {
-    mermaid.initialize({startOnLoad:true});
-    mermaid.init(undefined, document.querySelectorAll('.mermaid'));
-  },
-};
+    const graph = new Graph({
+      container: this.$refs.container,
+      grid: true
+    })
+
+    graph.addNode({
+      x: 80,
+      y: 80,
+      width: 100,
+      height: 40,
+      label: 'Hello',
+    })
+
+    graph.addNode({
+      x: 240,
+      y: 240,
+      width: 100,
+      height: 40,
+      label: 'X6',
+    })
+  }
+}
 </script>
+
+<style scoped>
+div {
+  width: 800px;
+  height: 600px;
+  background-color: #f5f5f5;
+}
+</style>
